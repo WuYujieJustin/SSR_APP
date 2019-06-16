638928f58c469108c35fc10ebfcf8a02589f773b
justin <ywu29@utas.edu.au>
2019-06-16 12:22:50 +0800

    reorganize code

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,59 @@
 <template>
-  <div class="container">
-    <Postcard v-for="post in posts" :key="post.id" :post="post"></Postcard>
-    <p>{{posts[0].id}}</p>
+  <div>
+    <!-- narbar layout -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Expand at md</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarsExample04"
+        aria-controls="navbarsExample04"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample04">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="dropdown04"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown04">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+          <input class="form-control" type="text" placeholder="Search">
+        </form>
+      </div>
+    </nav>
+    <div class="container">
+      <Postcard v-for="post in posts" :key="post.id" :post="post"></Postcard>
+    </div>
+
     <button @click="getPost">add </button>
   </div>
 </template>
@@ -13,14 +65,25 @@ export default {
   components: {
     Postcard
   },
-  computed: {
-    posts() {
-      return this.$store.state.post.posts;
-    }
+  data(){
+    return{
+      posts:[]
+  }},
+  created() {
+    // axios.get(' http://localhost:4000/posts',(res)=>{
+    //  console.log('123')
+    // //   this.posts = res.data
+    // // })
+    // console.log(axios)
+    axios.get('http://localhost:4000/posts')
+      .then((res)=>{
+        this.posts = res.data
+      })
+    
   },
   methods: {
-    getPost(){
-      this.$store.dispatch('post/getPost');
+    getPost() {
+      this.$store.dispatch("post/newPost");
     }
   }
 };
--- store/post.js
+++ store/post.js
@@ -1,29 +1,31 @@
 import axios from 'axios';
 
 export const state = () => ({
-    "posts": [{
-        "id": 1,
-        "title": "test title",
-        "content": "test content"
-    }]
+    "posts": []
 })
 
 
 export const mutations = {
-    getPost(state) {
-        state.posts[0].id++
+    newPost(state) {
+        
     }
 }
 
 export const actions = {
     // async getPost(state) {
     //     // const res = await axios.get(`http://localhost:4000/posts`);
-    //     // state.posts = res.data;
     //     const res = await axios.get('http://localhost:4000/posts');
     //     state.posts = res.data;
-    getPost({ commit }) {
+    newPost({ commit }) {
         setTimeout(function () {
-            commit(getPost), 3000
+            console.log("success")
+            commit('newPost')
+        }, 3000)
+    },
+    getPosts({commit}){
+        axios.get('http://localhost:4000/posts',(res)=>{
+            console.log(res.data)
+            commit('getPosts')
         })
     }
 }
